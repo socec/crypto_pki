74b98e8898161750412d36a7c106abe98b0ce188
Igor Socec <igor@debians>
2014-05-08 14:48:37 +0200

    update

--- certificates.py
+++ certificates.py
@@ -1,18 +1,18 @@
 from Crypto.PublicKey import RSA
 
 def generate_RSA_keypair():
-	from Crypto import Random 
+	from Crypto import Random
 	key = RSA.generate(1024, Random.new().read) 
 	return key
 
 def save_public_key(public_key_file, key):
 	f = open(public_key_file, 'wb')
-	f.write(key.publickey().exportKey())
+	f.write(key.publickey().exportKey('OpenSSH'))
 	f.close
 
 def save_private_key(private_key_file, key):
 	f = open(private_key_file, 'wb')
-	f.write(key.exportKey())
+	f.write(key.exportKey('PEM'))
 	f.close
 
 def load_public_key(public_key_file):
@@ -31,20 +31,25 @@ def create_certificate(id_tag, public_key):
 	return a
 
 
-pub_loc = "pub.key"
-prv_loc = "prv.key"
+pub_key_path = "pub.key"
+prv_key_path = "prv.key"
 
 key = generate_RSA_keypair()
-save_public_key(pub_loc, key)
-save_private_key(prv_loc, key)
+save_public_key(pub_key_path, key)
+save_private_key(prv_key_path, key)
 
 data = "abcdefgh"
 print data
 
-pubkey = load_public_key(pub_loc)
+pubkey = load_public_key(pub_key_path)
 edata = pubkey.encrypt(data, 32)
 print edata[0]
 
-prvkey = load_private_key(prv_loc)
+prvkey = load_private_key(prv_key_path)
 ddata = prvkey.decrypt(edata)
 print ddata
+
+f = open(pub_key_path, 'r')
+a = f.read()
+print a
+f.close
---
+++ certificates2.py
@@ -0,0 +1,4 @@
+# http://pythonhosted.org//pyOpenSSL/api.html
+# http://pki-tutorial.readthedocs.org/en/latest/
+
+
